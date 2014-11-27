9541147df3a860fe7653ea270e1980cfd2ff585a
pratt.service.2.0.au@gmail.com <pratt.service.2.0.au@gmail.com>
2014-11-27 13:01:13 +0530

    Readme update

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -63,11 +63,6 @@ return array(
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
-        'template_map' => array(
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ),
          'template_path_stack' => array(
             'admin' => __DIR__ . '/../view/',
             'paginator-slide' => __DIR__ . '/../view/layout/slidePaginator.phtml',
